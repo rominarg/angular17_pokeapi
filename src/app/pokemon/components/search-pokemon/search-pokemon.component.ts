f56ec884f4ec389f418e05faef0b3556d772138c
RominaNavarrete <rominagnavarrete@gmail.com>
2024-04-21 23:33:32 -0300

    creacion de componentes

---
+++ src/app/pokemon/components/search-pokemon/search-pokemon.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-pokemon',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-pokemon.component.html',
+  styleUrl: './search-pokemon.component.css'
+})
+export class SearchPokemonComponent {
+
+}
